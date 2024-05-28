9a4b9368901a321e2c266b3a33eb059f7b29cbcb
Gia Bao <thainguyengiabao27092004@gmail.com>
2024-05-28 20:42:13 +0700

    oik

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
-    <div >
+    <div>
       <h1>Hello world</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
